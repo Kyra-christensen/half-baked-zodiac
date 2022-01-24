b1394011e667794f37b165b5139f596a04a1374f
Kyra Christensen <christensen.kyral@gmail.com>
2022-01-24 15:06:51 -0800

    functions are working and zodiac card css is working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css'
 {/* import the zodiac array */}
+import { zodiac } from './data'
+import Main from './Main/Main.js'
+import Header from './Header/Header.js'
+import Footer from './Footer/Footer.js'
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
+      
       {/* the Header component needs a string passed in as the `name` prop */}
+      <Header name="Kyra Christensen" />
       {/* the Footer component needs a year passed in as the `year` prop */}
+      <Footer year="2022" />
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
+      <Main zodiacs={zodiac}/>
     </div>
   )
 }
